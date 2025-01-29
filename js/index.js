fd070229b14a059cd07a276048ed5b86ce169446
Corgi <hatsunemiku39exe@gmail.com>
2025-01-29 14:36:39 -0600

    Try to run it :D

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 function testGithubSecret() {
     document.getElementById("testingVariablePass").textContent = "Just test the content"
 
-    fetch("newFile.txt")
+    fetch("./newFile.txt")
         .then((response) => response.text())
         .then((text) =>  {
             console.log(text)
         })
         .catch((e) => console.error(e))
 }
+
+testGithubSecret()
