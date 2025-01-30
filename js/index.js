f9b39f6fc35d14ada53912e0c33f67fda6b17a60
Corgi <hatsunemiku39exe@gmail.com>
2025-01-29 20:11:37 -0600

    Await return

--- js/index.js
+++ js/index.js
@@ -12,13 +12,14 @@ function changeImage() {
     document.getElementById("FraeyaImage").src = images[Math.floor(Math.random() * images.length)]
 }
 
+const vodJson = await pullFraeyaVods()
 
 function pullFraeyaVods() {
-    let vodJson = {}
+    
     fetch("./js/newFile.txt")
         .then((response) => response.text())
         .then((text) =>  {
-            vodJson = JSON.parse(text)
+            return JSON.parse(text)
         })
         .catch((e) => console.error(e))
 
