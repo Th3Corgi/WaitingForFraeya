c22f6a06f6871767cafeea9cb6fd735fd7b00271
Corgi <hatsunemiku39exe@gmail.com>
2025-02-12 16:59:24 -0600

    Change where we are savving the audio

--- audio/prepareAudios.py
+++ audio/prepareAudios.py
@@ -7,5 +7,5 @@ audios = list(filter(lambda x: x.endswith("mp3"), listdir("./audio") ))
 print(json.dumps(audios))
 
 # Dump the json list into a new file
-with open("audio\audioList.json", "w+") as f:
+with open("./audio/audioList.json", "w+") as f:
     f.write(json.dumps(audios))
